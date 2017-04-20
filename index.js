a1530a333a8de8c79ed3b05c2a56c6bed797cb96
andrestein <andresgrandapiedrahita@gmail.com>
2017-04-19 21:56:01 -0500

    solucionando servidor

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function atenderServidor(request,response){
 }
 
 function retornarArchivo(request,response){
-  fs.readFile("./files/topaco/index.html",archivolisto);
+  fs.readFile(request.url+"/files/topaco/index.html",archivolisto);
 
   function archivolisto(err,data){
     if(err == null){
