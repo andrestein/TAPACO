f0638c457117f1eac38cc45e85dbe61647c98341
ThomasGil <thomasgiln@hotmail.com>
2017-05-05 21:52:01 -0500

    agregada funcionalidad para que no pueda entrar a formulario y cotizaciones si no ha iniciado sesion

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -4,6 +4,8 @@
     var fullCookies = [];
     var cookies = [];
 
+    var url = window.location.pathname.split(".");
+
     fullCookies = document.cookie.split(';');
 
     for(var i = 0; i < fullCookies.length; i++){
@@ -14,24 +16,26 @@
 
     function loadOptions(){
 
-    for(var i = 0; i < cookies.length - 1 ; i +=2){
-      if(cookies[i] == 'usuario' && cookies[i+1] != 'null'){
+    if(verificarAutenticidad()){
+
+      for(var i = 0; i < cookies.length - 1 ; i +=2){
+        if(cookies[i] == 'usuario' && cookies[i+1] != 'null'){
+
+            user = cookies[i+1];
+
+            document.getElementById("prueba").innerHTML = "<li><a href='formulario.html'> Hacer cotizacion </a></li>"
+            + "<li><a onclick='cerrarSesion()'> Cerrar sesion </a></li>"
+            + "<li class='dropdown-header'Nav header</li>"
+            + "<li role='separator' class='divider'></li>"
+            + "<li><a href='#'>Preguntas Frecuentes</a></li>"
+            + "<li><a href='#'>Terminos y Condiciones</a></li>"; 
+
+        }
+        else if(cookies[i] == 'asesor' && cookies[i+1] != 'null'){
 
           user = cookies[i+1];
 
-          document.getElementById("prueba").innerHTML = "<li><a href='formulario.html'> Hacer cotizacion </a></li>"
-          + "<li><a onclick='cerrarSesion()'> Cerrar sesion </a></li>"
-          + "<li class='dropdown-header'Nav header</li>"
-          + "<li role='separator' class='divider'></li>"
-          + "<li><a href='#'>Preguntas Frecuentes</a></li>"
-          + "<li><a href='#'>Terminos y Condiciones</a></li>"; 
-
-      }
-      else if(cookies[i] == 'asesor' && cookies[i+1] != 'null'){
-
-          user = cookies[i+1];
-
-          document.getElementById("prueba").innerHTML = "<li><a href='verPedidos.html'> Ver cotizaciones </a></li>"
+          document.getElementById("prueba").innerHTML = "<li><a href='verCotizaciones.html'> Ver cotizaciones </a></li>"
           + " <li><a onclick='cerrarSesion()'> Cerrar sesion </a></li>"
           + "<li class='dropdown-header'Nav header</li>"
           + "<li role='separator' class='divider'></li>"
@@ -40,6 +44,24 @@
 
         }
       }
+    }
+    else{
+      window.alert("Ud no puede estar en esta pagina sin iniciar sesion");
+      window.location.href = "index.html";
+    }
+  }
+
+    function verificarAutenticidad(){
+      
+      if(url[0] == "/verCotizaciones" || url[0] == "/formulario"){
+        for(var i = 0; i < cookies.length - 3 ; i +=2){
+          if(cookies[i+1] == 'null' && cookies[i+3] == 'null'){
+            //no hay ningun usuario o asesor regitrado con sesion activa
+            return false;
+          }
+        }
+      }
+      return true;
     }
 
     function cerrarSesion(){
@@ -52,7 +74,7 @@
 
       // Crear un objeto para hacer peticiones HTTP
       var request = new XMLHttpRequest();
-      var url = window.location.pathname.split(".");
+
       // Abrir una peticion HTTP POST a la url deseado y no asincronico
       request.open( "POST", url[0] , false );
     
